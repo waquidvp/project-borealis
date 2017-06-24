30a51dac05242ecd8bdcc8086ef251e1214d9fda
ArchieGodfrey <archiegod@gmail.com>
2017-06-24 16:33:04 +0100

    Added textInputs to sign-in page

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,22 +1,52 @@
 import React from 'react';
-import { View, Text,TextInput } from 'react-native';
-import styled from 'styled-components';
+import { View, Text,TextInput,Dimensions } from 'react-native';
+import styled from 'styled-components/native';
+
+
+const Screen = {
+  width: Dimensions.get('window').width,
+  height: Dimensions.get('window').height
+}
 
 const MainContainer = styled.View`
     flex: 1;
-    background-color: red;
-    width: ${props => props.small ? 45 : 50}
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    flex-wrap:wrap;
+    background-color: #243342;
 `;
 
 const InputBox = styled.TextInput`
-    flex:2;
+    color: white;
+    fontSize:20;
 `;
+
+class UserInput extends React.Component {
+    render() {
+        return( 
+            <InputBox style={{ color: "white",
+            paddingBottom: Screen.width / 8, paddingLeft: Screen.width / 4}}
+            placeholder={this.props.Message} 
+            onChange={(text) => this.setState({text})} >
+            </InputBox>
+        )
+    }
+}
 
 export default class SignIn extends React.Component {
     render(){
         return(
-            <MainContainer small>
-                <InputBox></InputBox>
+            <MainContainer>
+                <Text style={{ color: "white", fontSize:40,
+                marginBottom: 100,
+                paddingLeft: Screen.width / 4}}
+                >Fitness Riot</Text>
+                <View>
+                    <UserInput Message={'Enter your email address'}/>
+                    <UserInput Message={'Enter a password'}/>
+                    <UserInput Message={'Enter your password again'}/>
+                </View>
             </MainContainer>
         );
     }
